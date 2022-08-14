b7bc39569b54f75e6dad842d21b636b95fbbf5d6
Zannatul-Fahmida <jannatulfahmida15@gmail.com>
2022-08-14 11:23:27 +0600

    day-3 complete

---
+++ day-3.js
@@ -0,0 +1,8 @@
+function areaOfTriangle(h, b) {
+    const area = (h * b) / 2;
+    return area;
+}
+const triangle = areaOfTriangle(7, 4);
+console.log(triangle);
+const triangle2 = areaOfTriangle(10, 10);
+console.log(triangle2);
